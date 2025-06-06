3a439a14c5e15a5af809ea8e7924065ea239b0e1
(axa) <(a.x.a@inbox.ru)>
2025-06-07 02:25:15 +1100

    go version 1.22

--- randstring.go
+++ randstring.go
@@ -7,54 +7,54 @@ import (
 )
 
 const (
-  // minimum length
-  minLength = 1
+	// minimum length
+	minLength = 1
 )
 
 var (
-  defaultCharSet = []string{
-    "abcdefghijklmnopqrstuvwxyz",
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "0123456789",
-    "~!@#$%^&*()-_=+[{]};:\\|/"}
+	defaultCharSet = []string{
+		"abcdefghijklmnopqrstuvwxyz",
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		"0123456789",
+		"~!@#$%^&*()-_=+[{]};:\\|/"}
 )
 
 func Generate(length int, charSet ...string) string {
-  if len(charSet) == 0 || len(charSet[0]) == 0 {
-    charSet = defaultCharSet
-  }
+	if len(charSet) == 0 || len(charSet[0]) == 0 {
+		charSet = defaultCharSet
+	}
 
-  if length < minLength {
-    length = minLength
-  }
+	if length < minLength {
+		length = minLength
+	}
 
-  rnd := rand.NewSource(time.Now().Unix())
-  var sb strings.Builder
+	rnd := rand.NewSource(time.Now().Unix())
+	var sb strings.Builder
 
-  for _, s := range charSet {
-    if length == 0 {
-      break
-    }
+	for _, s := range charSet {
+		if length == 0 {
+			break
+		}
 
-    n := len(s)
-    rndn := int(rnd.Int63())
-    sb.WriteByte(s[rndn%n])
-    length--
-  }
+		n := len(s)
+		rndn := int(rnd.Int63())
+		sb.WriteByte(s[rndn%n])
+		length--
+	}
 
-  s := strings.Join(charSet, "")
+	s := strings.Join(charSet, "")
 
-  n := len(s)
-  for length > 0 {
-    rndn := int(rnd.Int63())
-    sb.WriteByte(s[rndn%n])
-    length--
-  }
+	n := len(s)
+	for length > 0 {
+		rndn := int(rnd.Int63())
+		sb.WriteByte(s[rndn%n])
+		length--
+	}
 
-  p := []byte(sb.String())
-  rand.Shuffle(len(p), func(i, j int) {
-    p[i], p[j] = p[j], p[i]
-  })
+	p := []byte(sb.String())
+	rand.Shuffle(len(p), func(i, j int) {
+		p[i], p[j] = p[j], p[i]
+	})
 
-  return string(p)
+	return string(p)
 }
